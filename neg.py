4d2b4ea1f7cedbca75d087ddcf78269696a6b05f
lararodriguez04 <162717767+lararodriguez04@users.noreply.github.com>
2024-04-27 20:59:34 +0200

    Update neg.py

--- neg.py
+++ neg.py
@@ -39,7 +39,7 @@ class MedicalReportTagger:
             scope_start, scope_end = self.find_negation_scope(text, start, end)
             # Create a result for the scope of negation
             results.append({
-                "value": {"start": end, "end": scope_end+1, "labels": ["NSCO"]},
+                "value": {"start": end+1, "end": scope_end+1, "labels": ["NSCO"]},
                 "id": f"scope{scope_id}",
                 "from_name": "label",
                 "to_name": "text",
@@ -64,7 +64,7 @@ class MedicalReportTagger:
             scope_start, scope_end = self.find_uncertainty_scope(text, start, end)
             # Create a result for the scope of uncertainty
             results.append({
-                "value": {"start": end, "end": scope_end+1, "labels": ["USCO"]},
+                "value": {"start": end+1, "end": scope_end+1, "labels": ["USCO"]},
                 "id": f"scope{scope_id}",
                 "from_name": "label",
                 "to_name": "text",
